0329def81a5485ad19a3829a54bed6ba77935353
KarenChobanyan <karen.ysu7@mail.ru>
2024-07-03 10:16:06 +0400

    Done

--- src/Pages/Home/homePage-hooks.tsx
+++ src/Pages/Home/homePage-hooks.tsx
@@ -1,10 +1,11 @@
 import { IActionCard } from "../../Interfaces/componentTypes";
 import { BetweenStorages, Sales, OutFromStorage, IntoStorage, CashIncome, CashOut, CashChange, Users, StorageRemove, ReturnableImage } from '../../Assets/Images';
-import { useGeneralHooks } from "../../General/Hooks/hooks";
-import { IGetMeResponseData } from "../../Interfaces/responseTypes";
+import { useAutocompleteData, useGeneralHooks } from "../../General/Hooks/hooks";
+import { toast } from "react-toastify";
 
 const useHomePageHooks = () => {
-    const { t, navigate,currentUser } = useGeneralHooks();
+    const { t, navigate, currentUser } = useGeneralHooks();
+    const { myWarehousesData, cashRegistersData } = useAutocompleteData()
     const getOPpionList = (id: number): IActionCard[] | [] => {
         switch (id) {
             case 1:
@@ -25,42 +26,91 @@ const useHomePageHooks = () => {
                     {
                         src: `${IntoStorage}`,
                         title: t('Actions.To_Storage.Title'),
-                        onClick: () => { navigate('/storage_incomings') }
+                        onClick: () => {
+                            if (myWarehousesData?.length! > 0) {
+                                navigate('/storage_incomings')
+                            } else {
+                                toast.warning(t('Toast.Warning.No_Warehouse'))
+                            }
+                        }
                     },
                     {
                         src: `${OutFromStorage}`,
                         title: t('Actions.From_Storage.Title'),
-                        onClick: () => { navigate('/storage_outgoings') }
+                        onClick: () => {
+                            if (myWarehousesData?.length! > 0) {
+                                navigate('/storage_outgoings')
+                            } else {
+                                toast.warning(t('Toast.Warning.No_Warehouse'))
+                            }
+                        }
                     },
                     {
                         src: `${BetweenStorages}`,
                         title: t('Actions.Between_Storages.Title'),
-                        onClick: () => { navigate('/storage_transfers') }
+                        onClick: () => {
+                            if (myWarehousesData?.length! > 0) {
+                                navigate('/storage_transfers')
+                            } else {
+                                toast.warning(t('Toast.Warning.No_Warehouse'))
+                            }
+                        }
                     },
                     {
                         src: `${Sales}`,
                         title: t('Actions.Sales.Title'),
-                        onClick: () => { navigate('/sales') }
+                        onClick: () => {
+                            if (myWarehousesData?.length! > 0) {
+                                navigate('/sales')
+                            } else {
+                                toast.warning(t('Toast.Warning.No_Warehouse'))
+                            }
+                        }
                     },
                     {
                         src: `${CashIncome}`,
                         title: t('Actions.Cash_Income.Title'),
-                        onClick: () => { navigate('/cash_incomings') }
+                        onClick: () => {
+                            if (cashRegistersData?.length! > 0) {
+                                navigate('/cash_incomings')
+                            } else {
+                                toast.warning(t('Toast.Warning.No_CashRegister'))
+                            }
+                        }
                     },
                     {
                         src: `${CashOut}`,
                         title: t('Actions.CashOut.Title'),
-                        onClick: () => { navigate('/cashouts') }
+                        onClick: () => {
+                            if (cashRegistersData?.length! > 0) {
+                                navigate('/cashouts')
+                            } else {
+                                toast.warning(t('Toast.Warning.No_CashRegister'))
+                            }
+
+                        }
                     },
                     {
                         src: `${CashChange}`,
                         title: t('Actions.Cash_Changes.Title'),
-                        onClick: () => { navigate('/cash_transfers') }
+                        onClick: () => {
+                            if (cashRegistersData?.length! > 0) {
+                                navigate('/cash_transfers')
+                            } else {
+                                toast.warning(t('Toast.Warning.No_CashRegister'))
+                            }
+                        }
                     },
                     {
                         src: `${StorageRemove}`,
                         title: t('Actions.Warehouse_Return.Title'),
-                        onClick: () => { navigate('/warehouse_returns') }
+                        onClick: () => { 
+                            if (myWarehousesData?.length! > 0) {
+                                navigate('/warehouse_returns')
+                             }else{
+                                toast.warning(t('Toast.Warning.No_Warehouse'))
+                             }
+                            }
                     },
                 ]
             default:
